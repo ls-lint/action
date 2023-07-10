0ee62a23e017386453e10173f84dfe2cc6fa5595
Lucas Löffel <lucas@loeffel.io>
2023-07-10 11:27:16 +0200

    feat: add args

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,10 @@ const path = require('path');
 const child = spawn(
     path.join(__dirname, "../node_modules/@ls-lint/ls-lint/bin/cli.js"),
     [
-        `--config=${core.getInput("config")}`
+        `--config=${core.getInput("config")}`,
+        `--workdir=${core.getInput("workdir")}`,
+        `--debug=${core.getInput("debug")}`,
+        `--warn=${core.getInput("warn")}`
     ],
     {stdio: [process.stdin, process.stdout, process.stderr]}
 );
