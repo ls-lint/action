9c0fa03d6eba47e4db04d3565f4227d1ed056ee2
Lucas Löffel <lucas@loeffel.io>
2024-07-15 17:19:15 +0200

    test: input

--- bin/index.js
+++ bin/index.js
@@ -4,18 +4,15 @@ const core = require('@actions/core');
 const spawn = require('child_process').spawn;
 const path = require('path');
 
-const argConfig = core.getInput("config").trim().split(' ').map(value => `--config=${value}`)
-const argPaths = core.getInput("paths").trim().split(' ')
-
 const child = spawn(
     path.join(__dirname, "../node_modules/@ls-lint/ls-lint/bin/cli.js"),
     [
-        ...argConfig,
+        ...core.getInput("config").trim().split(' ').map(value => `--config=${value}`),
         `--workdir=${core.getInput("workdir")}`,
         `--error-output-format=${core.getInput("error-output-format")}`,
         `--debug=${core.getInput("debug")}`,
         `--warn=${core.getInput("warn")}`,
-        ...argPaths
+        ...core.getInput("paths").trim().split(' ')
     ],
     {stdio: [process.stdin, process.stdout, process.stderr]}
 );
