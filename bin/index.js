83e328429089969e32f47857b16b913b0ec59e5c
Lucas Löffel <lucas@loeffel.io>
2024-07-15 17:48:12 +0200

    test: inherit

--- bin/index.js
+++ bin/index.js
@@ -4,17 +4,19 @@ const core = require('@actions/core');
 const spawn = require('child_process').spawn;
 const path = require('path');
 
+const args = [
+    ...core.getInput("config").trim().split(' ').map(value => `--config=${value}`),
+    `--workdir=${core.getInput("workdir")}`,
+    `--error-output-format=${core.getInput("error-output-format")}`,
+    `--debug=${core.getInput("debug")}`,
+    `--warn=${core.getInput("warn")}`,
+    ...core.getInput("paths").trim().split(' ')
+];
+
 const child = spawn(
     path.join(__dirname, "../node_modules/@ls-lint/ls-lint/bin/cli.js"),
-    [
-        ...core.getInput("config").trim().split(' ').map(value => `--config=${value}`),
-        `--workdir=${core.getInput("workdir")}`,
-        `--error-output-format=${core.getInput("error-output-format")}`,
-        `--debug=${core.getInput("debug")}`,
-        `--warn=${core.getInput("warn")}`,
-        ...core.getInput("paths").trim().split(' ')
-    ],
-    {stdio: [process.stdin, process.stdout, process.stderr]}
+    args,
+    {stdio: 'inherit'}
 );
 
 child.on('close', function (code) {
